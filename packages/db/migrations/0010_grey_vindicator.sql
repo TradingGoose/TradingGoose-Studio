64d8c85c40f0afead58d941fce8715020b178248
Bruzzz <brucewj2310@gmail.com>
2026-02-20 10:43:01 -0700

    refactor(db): remove redundant order history listing columns

---
+++ packages/db/migrations/0010_grey_vindicator.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "orderHistoryTable" DROP COLUMN "listing_id";--> statement-breakpoint
+ALTER TABLE "orderHistoryTable" DROP COLUMN "listing_key";--> statement-breakpoint
+ALTER TABLE "orderHistoryTable" DROP COLUMN "listing_type";
