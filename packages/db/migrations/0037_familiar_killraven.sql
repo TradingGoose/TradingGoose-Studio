5c9f41b8f310d200c0b271af27fbc4a4a2235e7b
BWJ2310-backup <jun.1216.wei@gmail.com>
2026-07-02 20:07:29 -0600

    build(db): add migration 0037_familiar_killraven
    Co-authored-by: BWJ2310 <brucewj2310@gmail.com>
    
    Co-authored-by: BWJ2310-backup <jun.1216.wei@gmail.com>

---
+++ packages/db/migrations/0037_familiar_killraven.sql
@@ -0,0 +1,5 @@
+DROP TABLE "marketplace" CASCADE;--> statement-breakpoint
+DROP TABLE "template_stars" CASCADE;--> statement-breakpoint
+DROP TABLE "templates" CASCADE;--> statement-breakpoint
+ALTER TABLE "workflow" DROP COLUMN "is_published";--> statement-breakpoint
+ALTER TABLE "workflow" DROP COLUMN "marketplace_data";
